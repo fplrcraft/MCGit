0aae5315ec523e2107e3c3ad48f55cef9e2ffc60
Jeb Feng <ranzeplay@outlook.com>
2020-03-22 11:17:34 +0800

    Update plugin.yml, add .gitignore, add default config generator

--- src/main/java/me/ranzeplay/mcgit/Main.java
+++ src/main/java/me/ranzeplay/mcgit/Main.java
@@ -1,17 +1,37 @@
 package me.ranzeplay.mcgit;
 
+import me.ranzeplay.mcgit.commands.CommandCompleter;
+import me.ranzeplay.mcgit.commands.CommandExec;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.Objects;
+
 public final class Main extends JavaPlugin {
+
+    public static Main Instance = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Instance = this;
+        this.initialPluginFiles();
 
+        Objects.requireNonNull(Bukkit.getPluginCommand("mcgit")).setExecutor(new CommandExec());
+        Objects.requireNonNull(Bukkit.getPluginCommand("mcgit")).setTabCompleter(new CommandCompleter());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void initialPluginFiles() {
+        saveDefaultConfig();
+
+        File path = getDataFolder();
+        File backupFolder = new File(path + "\\Backups");
+        if (!backupFolder.exists()) backupFolder.mkdirs();
     }
 }
