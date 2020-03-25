50dc971490ee86da3bb162ecc0a4ae99d5ae50d5
Jeb Feng <ranzeplay@outlook.com>
2020-03-25 20:57:24 +0800

    Save commit data to a individual yml file, can view them using commands

--- src/main/java/me/ranzeplay/mcgit/commands/CommandExec.java
+++ src/main/java/me/ranzeplay/mcgit/commands/CommandExec.java
@@ -1,30 +1,45 @@
 package me.ranzeplay.mcgit.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.ParseException;
 
 public class CommandExec implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (command.isRegistered()) {
-            if (command.getName().equalsIgnoreCase("mcgit") || command.getAliases().contains(s)) {
-                if (args.length > 0) {
-                    switch (args[0].toLowerCase()) {
-                        case "commit":
-                            try {
-                                Commit.Do(args, commandSender);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            break;
-                        case "rollback":
-                            break;
+            if(commandSender instanceof Player) {
+                if (command.getName().equalsIgnoreCase("mcgit") || command.getAliases().contains(s)) {
+                    if (args.length > 0) {
+                        switch (args[0].toLowerCase()) {
+                            case "commit":
+                                try {
+                                    CommitCommand.Do(args, commandSender);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "view":
+                                try {
+                                    ViewCommand.Do(args, commandSender);
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            case "rollback":
+                                break;
+                        }
                     }
-                }
 
-                return true;
+                    return true;
+                }
             }
+
+            commandSender.sendMessage(ChatColor.RED + "The command can only be executed by a Player");
         }
 
         return false;
