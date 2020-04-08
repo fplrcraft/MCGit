6f0b0f5ab3aa63a89aee309d0cc1d516965cebbe
Jeb Feng <ranzeplay@outlook.com>
2020-04-08 10:50:27 +0800

    Create a simple GUI to display all commits, player can also do operations with it.

---
+++ src/main/java/me/ranzeplay/mcgit/gui/CommitsPanel.java
@@ -0,0 +1,76 @@
+package me.ranzeplay.mcgit.gui;
+
+import me.ranzeplay.mcgit.commands.ViewCommand;
+import me.ranzeplay.mcgit.managers.GitManager;
+import me.ranzeplay.mcgit.models.Commit;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class CommitsPanel implements InventoryHolder, Listener {
+    private Inventory inventory;
+
+    public Inventory getInventory() {
+        ArrayList<Commit> commitsList = null;
+        try {
+            commitsList = GitManager.commitsList();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        inventory = Bukkit.createInventory(null, 9 * 6, "All Commits");
+        int i = 1;
+        for (Commit commit : commitsList) {
+            if (i >= 54) {
+                break;
+            }
+            i++;
+
+            ItemStack item = new ItemStack(Material.GREEN_WOOL, 1);
+            ItemMeta meta = item.getItemMeta().clone();
+            meta.setDisplayName(commit.getCommitId().toString());
+
+            ArrayList<String> lore = new ArrayList<>();
+            lore.add(ChatColor.GREEN + "Description: " + ChatColor.YELLOW + commit.getDescription());
+            lore.add(ChatColor.GREEN + "Create Time: " + ChatColor.YELLOW + commit.getCreateTime());
+            lore.add(ChatColor.GREEN + "Operator: " + ChatColor.YELLOW + commit.getPlayer().getName());
+            lore.add(ChatColor.GREEN + "World Name: " + ChatColor.YELLOW + commit.getWorld());
+            meta.setLore(lore);
+
+            item.setItemMeta(meta);
+            inventory.addItem(item);
+        }
+
+        return inventory;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) throws ParseException {
+        if (event.getClickedInventory().getHolder() != this.getInventory().getHolder()) {
+            return;
+        }
+
+        Player player = (Player) event.getWhoClicked();
+        String commitId = event.getCurrentItem().getItemMeta().getDisplayName();
+        if (event.getClick().isLeftClick()) {
+            player.closeInventory();
+            ViewCommand.ViewCommit(player, commitId);
+        } else if(event.getClick().isRightClick()) {
+            player.closeInventory();
+            player.performCommand("mcgit rollback " + commitId);
+        }
+
+        event.setCancelled(true);
+    }
+}
